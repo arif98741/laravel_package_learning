9edfe5a61915077bf2c4c7c1c4e0002f397fd4b5
Ariful <arif98741@gmail.com>
2019-11-07 16:51:59 +0600

    first

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace phpdark\Press\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+
+    
+}
